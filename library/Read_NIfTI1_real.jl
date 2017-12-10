f4a474defb971db9223906397656bea1a9c00c8b
nankueichen <nkchen@email.arizona.edu>
2017-12-09 22:47:23 -0700

    update nifti library

--- library/Read_NIfTI1_real.jl
+++ library/Read_NIfTI1_real.jl
@@ -1,14 +1,113 @@
-module Read_nifti1
+module Read_NIfTI1_real
  
     export load_nii_header
     export write_nii_header
 #     export voxellocation!
 #     export inversevoxellocation!
     export load_nii_data
+    export load_niigz_header
+    export load_niigz_data
+    import GZip
 
+# See https://nifti.nimh.nih.gov/pub/dist/src/niftilib/nifti1.h 
     function load_nii_header(filename::String)    
         headerinfo = Dict()
         fid = open(filename,"r")
+        testvalue = read(fid,Int32,1);
+        headerinfo["sizeof_hdr"] = testvalue
+        testvalue = read(fid,UInt8,10);
+        headerinfo["data_type"] = testvalue
+        testvalue = read(fid,Int8,18);
+        headerinfo["db_name"] = testvalue
+        testvalue = read(fid,Int32,1);
+        headerinfo["extents"] = testvalue
+        testvalue = read(fid,Int16,1);
+        headerinfo["session_error"] = testvalue
+        testvalue = read(fid,Int8,1);
+        headerinfo["regular"] = testvalue
+        testvalue = read(fid,Int8,1);
+        headerinfo["dim_info"] = testvalue
+        testvalue = read(fid,Int16,8);
+        headerinfo["dim"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["intent_p1"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["intent_p2"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["intent_p3"] = testvalue
+        testvalue = read(fid,Int16,1);
+        headerinfo["intent_code"] = testvalue
+        testvalue = read(fid,Int16,1);
+        headerinfo["datatype"] = testvalue
+        testvalue = read(fid,Int16,1);
+        headerinfo["bitpix"] = testvalue
+        testvalue = read(fid,Int16,1);
+        headerinfo["slice_start"] = testvalue
+        testvalue = read(fid,Float32,8);
+        headerinfo["pixdim"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["vox_offset"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["scl_slope"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["scl_inter"] = testvalue
+        testvalue = read(fid,Int16,1);
+        headerinfo["slice_end"] = testvalue
+        testvalue = read(fid,Int8,1);
+        headerinfo["slice_code"] = testvalue
+        testvalue = read(fid,Int8,1);
+        headerinfo["xyzt_units"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["cal_max"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["cal_min"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["slice_duration"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["toffset"] = testvalue
+        testvalue = read(fid,Int32,1);
+        headerinfo["glmax"] = testvalue
+        testvalue = read(fid,Int32,1);
+        headerinfo["glmin"] = testvalue
+        testvalue = read(fid,Int8,80);
+        headerinfo["descrip"] = testvalue
+        testvalue = read(fid,Int8,24);
+        headerinfo["aux_file"] = testvalue
+        testvalue = read(fid,Int16,1);
+        headerinfo["qform_code"] = testvalue
+        testvalue = read(fid,Int16,1);
+        headerinfo["sform_code"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["quatern_b"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["quatern_c"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["quatern_d"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["qoffset_x"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["qoffset_y"] = testvalue
+        testvalue = read(fid,Float32,1);
+        headerinfo["qoffset_z"] = testvalue
+        testvalue = read(fid,Float32,4);
+        headerinfo["srow_x"] = testvalue
+        testvalue = read(fid,Float32,4);
+        headerinfo["srow_y"] = testvalue
+        testvalue = read(fid,Float32,4);
+        headerinfo["srow_z"] = testvalue
+        testvalue = read(fid,Int8,16);
+        headerinfo["intent_name"] = testvalue
+        testvalue = read(fid,Int8,4);
+        headerinfo["magic"] = testvalue
+        close(fid)
+        headerinfo["qfac"] = headerinfo["pixdim"][1];
+        headerinfo["filename"]= filename;
+        return headerinfo;
+    end
+
+    function load_niigz_header(filename::String)    
+        headerinfo = Dict()
+        fid = GZip.open(filename,"r")
         testvalue = read(fid,Int32,1);
         headerinfo["sizeof_hdr"] = testvalue
         testvalue = read(fid,Int8,10);
@@ -102,7 +201,7 @@ module Read_nifti1
     end
 
     function write_nii_header(filename::String, headerinfo)
-        fid = open(filename,"a")
+        fid = open(filename,"w")
         write(fid,headerinfo["sizeof_hdr"]);
         write(fid,headerinfo["data_type"]);
         write(fid,headerinfo["db_name"]);
@@ -268,6 +367,49 @@ module Read_nifti1
         end
         return data;
     end
+
+    function load_niigz_data(filename::String, headerinfo)
+        zdim::Int = headerinfo["dim"][4];
+        ydim::Int = headerinfo["dim"][3];
+        xdim::Int = headerinfo["dim"][2];
+        tdim::Int = headerinfo["dim"][5];
+        ndim::Int = headerinfo["dim"][1];
+        offset::Int = convert(Int,headerinfo["vox_offset"][1]);
+        fid = GZip.open(filename,"r")
+        seek(fid,offset)
+        if headerinfo["datatype"][1]  == 2 
+            data = read(fid,UInt8,xdim*ydim*zdim*tdim)
+        elseif headerinfo["datatype"][1]  == 4
+            data = read(fid,Int16,xdim*ydim*zdim*tdim)
+        elseif headerinfo["datatype"][1]  == 8 
+            data = read(fid,Int32,xdim*ydim*zdim*tdim)
+        elseif headerinfo["datatype"][1]  == 16 
+            data = read(fid,Float32,xdim*ydim*zdim*tdim)
+        elseif headerinfo["datatype"][1]  == 64 
+            data = read(fid,Float64,xdim*ydim*zdim*tdim)
+        elseif headerinfo["datatype"][1]  == 256 
+            data = read(fid,Int8,xdim*ydim*zdim*tdim)
+        elseif headerinfo["datatype"][1]  == 512 
+            data = read(fid,UInt16,xdim*ydim*zdim*tdim)
+        elseif headerinfo["datatype"][1]  == 768 
+            data = read(fid,UInt32,xdim*ydim*zdim*tdim)
+        elseif headerinfo["datatype"][1]  == 1024 
+            data = read(fid,Int64,xdim*ydim*zdim*tdim)
+        elseif headerinfo["datatype"][1]  == 1280 
+            data = read(fid,UInt64,xdim*ydim*zdim*tdim)
+        else
+            data = [];
+            println("-- nii data were not loaded properly --")
+        end
+        close(fid);
+        if ndim == 4
+            data = reshape(data,xdim,ydim,zdim,tdim);
+        elseif ndim == 3
+            data = reshape(data,xdim,ydim,zdim);
+        end
+        return data;
+    end
+
     # datatype:
     #define NIFTI_TYPE_UINT8           2 /! unsigned char. /
     #define NIFTI_TYPE_INT16           4 /! signed short. /
